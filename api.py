ba16b70159eadcc7e0d5d618caf4873846074dde
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2016-10-19 12:18:50 -0500

    events

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from bs4 import BeautifulSoup
 import requests
 import feedparser
 import os
@@ -29,6 +30,27 @@ def news():
             break
         count += 1
     return jsonify({"entries":entries})
+
+@app.route("/events")
+def events():
+    r = requests.get("http://planitpurple.northwestern.edu/#search=/2/1+3+2+9+11+5+10+6+4+8+7/1+3+4+5/")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    events = soup.select(".events")[0]
+    events = events.find_all("li")
+
+    events_json = []
+    for event in events:
+    	link = event.find("a")
+    	url = link['href']
+    	e = {}
+    	e['title'] = link.get_text().strip()
+    	e['url'] = "http://planitpurple.northwestern.edu" + url
+    	e['time'] = event.select(".event_time")[0].get_text().strip()
+    	e['category'] = event.select(".event_category")[0].get_text().strip()
+    	e['location'] = event.select(".event_location")[0].get_text().replace("- ", "").strip()
+    	events_json.append(e)
+
+    return jsonify({"entries":events_json})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
