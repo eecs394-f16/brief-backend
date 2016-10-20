22ed9e4740e791d3bddd4ac507c8cdaf56d4fcd1
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2016-10-19 21:20:45 -0500

    jk for realz this time

--- api.py
+++ api.py
@@ -4,54 +4,12 @@ from bs4 import BeautifulSoup
 import requests
 import feedparser
 import os
-from datetime import timedelta
-from flask import make_response, request, current_app
-from functools import update_wrapper
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-@crossdomain(origin='*')
+@cross_origin()
 @app.route("/weather")
 def weather():
     r = requests.get("http://api.openweathermap.org/data/2.5/weather", params={"q":"Evanston","APPID":"2632210dcc93cc19c2ee8b5fb2b59af9"})
@@ -59,7 +17,7 @@ def weather():
 
     return jsonify({"main":result["main"],"weather":result["weather"]})
 
-@crossdomain(origin='*')
+@cross_origin()
 @app.route("/news")
 def news():
     d = feedparser.parse('https://dailynorthwestern.com/feed/rss/')
@@ -77,7 +35,7 @@ def news():
         count += 1
     return jsonify({"entries":entries})
 
-@crossdomain(origin='*')
+@cross_origin()
 @app.route("/events")
 def events():
     r = requests.get("http://planitpurple.northwestern.edu/#search=/2/1+3+2+9+11+5+10+6+4+8+7/1+3+4+5/")
